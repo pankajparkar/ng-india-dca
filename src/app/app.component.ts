77c1b6758eae4db348dfd5919577ea8ea39c6243
Pankaj Parkar <parkarpankaj3@gmail.com>
2023-02-23 22:13:19 +0800

    1. Added simple card 2. used on app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { SimpleCardComponent } from './simple-card/simple-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  imports: [
+    SimpleCardComponent,
+  ]
 })
 export class AppComponent {
   title = 'ng-india-dca';
---
+++ src/app/simple-card/simple-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-simple-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './simple-card.component.html',
+  styleUrls: ['./simple-card.component.scss']
+})
+export class SimpleCardComponent {
+
+}
