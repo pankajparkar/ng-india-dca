d18ee1073554056d0aa680f7b67be1590b77947b
Pankaj Parkar <parkarpankaj3@gmail.com>
2023-02-24 22:07:58 +0530

    components made flat.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, Routes } from '@angular/router';
 import { AppComponent } from './app/app.component';
-import { RegularComponent } from './app/components/regular/regular.component';
+import { RegularComponent } from './app/components/regular.component';
 
 const routes: Routes = [
   { path: 'regular', component: RegularComponent },
