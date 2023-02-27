3d33e0604d9799c71cf015f6b5ccec5fe0386400
Pankaj Parkar <parkarpankaj3@gmail.com>
2023-02-27 22:26:41 +0530

    Fixed types.

--- src/app/components/regular.component.ts
+++ src/app/components/regular.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { SimpleCardComponent } from 'src/app/components/simple-card.component';
+import { NgxMultiselectModule } from '../multliselect/multiselect.module';
 
 @Component({
   selector: 'app-regular',
   standalone: true,
   imports: [
     SimpleCardComponent,
+    NgxMultiselectModule,
   ],
   template: `
     <app-simple-card appBlink appBold></app-simple-card>
+    
   `,
 })
 export class RegularComponent {
